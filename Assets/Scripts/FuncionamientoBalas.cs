aaf86738db734353957871872dd4dfd50edf8f6b
vbgil04 <vb.gil.rubio@alumnos.upm.es>
2024-10-04 16:27:56 +0200

    arreglado errores complilacion

--- Assets/Scripts/FuncionamientoBalas.cs
+++ Assets/Scripts/FuncionamientoBalas.cs
@@ -17,14 +17,9 @@ public class FuncionamientoBalas : MonoBehaviour
     }
     void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Disparable")) {
-            ObjetivosManager.Instance.fuera = true;
-            ObjetivosManager.Instance.Despawn();
-            Debug.Log("Objetivo destruido");
-            ObjetivosManager.Instance.fuera = false;
+
             gameObject.SetActive(false); 
             Debug.Log("Bala destruida");
-            ObjetivosManager.Instance.puntos ++;
-            Debug.Log("Puntos: " + ObjetivosManager.Instance.puntos);
         }
     }
 }
--- Assets/Scripts/FuncionamientoPistola.cs
+++ Assets/Scripts/FuncionamientoPistola.cs
@@ -12,7 +12,6 @@ public class FuncionamientoPistola : MonoBehaviour
     void Update()
     {
         Shoot();
-        Puntaje();
     }
     public void Shoot()
     {
@@ -24,10 +23,5 @@ public class FuncionamientoPistola : MonoBehaviour
             bullet.SetActive(true);
             bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
         }
-    }
-
-    void Puntaje() { // función para mostrar el puntaje en pantalla
-        // GameObject.FindObjectOfType<UnityEngine.UI.Text>().text = "Puntuación  " + ObjetivosManager.Instance.puntos;
-        GameObject.FindWithTag("Score").GetComponent<Text>().text = "Puntuación " + ObjetivosManager.Instance.puntos;
     }
 }
